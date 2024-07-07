97bf83bbbdad1b2be00fdc272065b64a3a63439f
albertng94 <albertng94@gmail.com>
2024-07-07 15:13:34 +0200

    Display stats/results content within "Summary" component.
    For the stats content:
    
    Derived function "createStats" is created to map through the "selected
    Answers" array and count the number of correct, wrong and skipped
    answers. The function then returns an object containing the "correct
    Ratio", "wrongRatio", and "skippedRatio" keys which contain the % of
    the corresponding type of answer rounded.
    
    Then const "stats" is created within the component function and executes
    the derived "createStats" funcion, thus containing the stats.
    
    Finally, those stats are used directly within the corresponding html
    content.
    
    For the results content:
    
    Another derived function "createResults" is used to map through the
    "selectedAnswers" array and return an array of objects containing the
    "questionIndex", "question", "answer", and "answerClassName" keys with
    its corresponding information.
    
    The variable "results" is created within the component function and
    executes "createResults" so that it contains the results information.
    
    Finally, the results array is looped by within the ol element thus
    creating a list of items containing the needed results information and
    giving it the necessary conditional styling.

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,9 +3,60 @@ import questions from "../questions";
 import { useContext } from "react";
 import { QuizStageContext } from "../store/quizStage-context";
 
+function createStats(selectedAnswers) {
+      
+    let correctAnswers = 0;
+    let wrongAnswers = 0;
+    let skippedAnswers = 0;
+
+    selectedAnswers.map((answer, answerIndex) => {
+        if (answer === "skipped") {
+            ++skippedAnswers;
+        } else if (answer === questions[answerIndex].answers[0]) {
+            ++correctAnswers;
+        } else {
+            ++wrongAnswers;
+        }
+    });
+
+    return {
+        correctRatio: `${Math.round((correctAnswers / 7) * 100)}%`, 
+        wrongRatio: `${Math.round((wrongAnswers / 7) * 100)}%`,
+        skippedRatio: `${Math.round((skippedAnswers / 7) * 100)}%`
+    }
+}; 
+
+function createResults(selectedAnswers) {
+    const results = selectedAnswers.map((answer, answerIndex) => {
+
+        let answerStatus;
+
+        if (answer === "skipped") {
+            answerStatus = answer;
+        } else if (answer === questions[answerIndex].answers[0]) {
+            answerStatus = "correct";
+        } else {
+            answerStatus = "wrong";
+        }
+
+        return {
+            questionIndex: answerIndex+1,
+            question: questions[answerIndex].text,
+            answer: answer,
+            answerClassName: `user-answer ${answerStatus}` 
+        }
+    });
+
+    return results;
+}
+
 export default function Summary() {
 
     const { selectedAnswers } = useContext(QuizStageContext);
+
+    const stats = createStats(selectedAnswers);
+
+    const results = createResults(selectedAnswers);
 
     return (
         <div id="summary">
@@ -13,26 +64,26 @@ export default function Summary() {
             <h2>QUIZ COMPLETED!</h2>
             <div id="summary-stats">
                 <div>
-                    <p className="number">50%</p>
+                    <p className="number">{stats.skippedRatio}</p>
                     <p className="text">SKIPPED</p>
                 </div>
                 <div>
-                    <p className="number">50%</p>
-                    <p className="text">SKIPPED</p>
+                    <p className="number">{stats.correctRatio}</p>
+                    <p className="text">ANSWERED CORRECTLY</p>
                 </div>
                 <div>
-                    <p className="number">50%</p>
-                    <p className="text">SKIPPED</p>
+                    <p className="number">{stats.wrongRatio}</p>
+                    <p className="text">ANSWERED INCORRECTLY</p>
                 </div>
             </div>
             <ol>
-                <li>
-                    <h3>1</h3>
-                    <p className="question">egfwegyugfyuwe</p>
-                    <p className="user-answer correct">yuuuuuuup</p>
-                </li>
-                <li></li>
-                <li></li>
+                {results.map((questionResult) => (
+                    <li key={questionResult.questionIndex}>
+                        <h3>{questionResult.questionIndex}</h3>
+                        <p className="question">{questionResult.question}</p>
+                        <p className={questionResult.answerClassName}>{questionResult.answer}</p>
+                    </li>
+                ))}
             </ol>
         </div>
     );
